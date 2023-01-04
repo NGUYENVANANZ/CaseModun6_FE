4516b0619ad2496bff8397b9ba0185a2b5fbdd87
Your Name <trangialochvtc@gmail.com>
2023-01-04 14:50:49 +0700

    add

--- src/app/FE/login/login.component.ts
+++ src/app/FE/login/login.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit{
+  ngOnInit(): void {
+  }
+  loginForm = new FormGroup({
+    username: new FormGroup("",Validators.required),
+    password: new FormGroup("",Validators.required)
+  })
 }
