bcb4e513bd4195f3af68a89a04f3094d46becac0
nguyenhaivanz <haivan090824@gmail.com>
2023-01-06 22:29:33 +0700

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
 
-
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthInterceptor} from "./auth.interceptor";
-import {NgxPaginationModule} from "ngx-pagination";
-=======
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +8,7 @@ import { AuthInterceptor } from './auth.interceptor';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
->>>>>>> a127993b3207a8320e2de4399a266df9e32dfce7
+
 
 @NgModule({
   declarations: [
@@ -33,15 +21,10 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     // NgxPaginationModule,
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
 
     ReactiveFormsModule,
     HttpClientModule
 
-=======
-    ReactiveFormsModule,
-    HttpClientModule
->>>>>>> a127993b3207a8320e2de4399a266df9e32dfce7
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
-import { Injectable } from '@angular/core';
-import {AccountService} from "./service/account/account.service";
-import {HttpEvent, HttpHandler, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
 
-=======
+
 import {AccountService} from "./service/account/account.service";
 import {HttpEvent, HttpHandler, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 @Injectable()
->>>>>>> a127993b3207a8320e2de4399a266df9e32dfce7
+
 export class AuthInterceptor {
 
 
@@ -25,8 +20,5 @@ export class AuthInterceptor {
     return next.handle(request);
 
   }
-<<<<<<< HEAD
-=======
 
->>>>>>> a127993b3207a8320e2de4399a266df9e32dfce7
 }
