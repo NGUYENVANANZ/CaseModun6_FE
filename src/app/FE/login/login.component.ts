5a478cbfc6ea71db71d6f5c7121686d1ca22a17f
Your Name <trangialochvtc@gmail.com>
2023-01-06 08:46:52 +0700

    loc

--- src/app/FE/login/login.component.ts
+++ src/app/FE/login/login.component.ts
@@ -1,25 +1,32 @@
-import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import { FormGroup, Validators} from "@angular/forms";
-import {LoginService} from "../../service/login/login.service";
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {LoginService} from "../../services/login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit{
-  constructor(private loginService:LoginService) {
-  }
+export class LoginComponent implements OnInit {
+
+  constructor(private loginService: LoginService, private router:Router) { }
+
   ngOnInit(): void {
   }
+
   loginForm = new FormGroup({
-    username: new FormGroup("",Validators.required),
-    password: new FormGroup("",Validators.required)
+    username: new FormControl("",Validators.required),
+    password: new FormControl("",Validators.required)
   })
+
   login(){
     this.loginService.login(this.loginForm.value).subscribe((data)=>{
-        this.loginService.setToken(data)
+      this.loginService.setUserToken(data);
+      this.loginService.setToken(data.token);
+      this.router.navigate(["/products"])
     })
   }
+
 
 }
--- src/app/service/login/login.service.ts
+++ src/app/service/login/login.service.ts
@@ -1,26 +1,34 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
 
+const API_URL = `${environment.apiUrl}`;
+import {Account} from "../../FE/model/Account";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private http:HttpClient) { }
 
-login(user:any):Observable<string>{
-    return this.http.post<string>("http://localhost:8080/login",user)
-}
+  constructor(private http: HttpClient) {
 
-  setToken(token:string){
-    localStorage.setItem("token",token)
   }
-  getToken(token:string){
+
+  login(account: Account): Observable<Account> {
+    return this.http.post<Account>(API_URL + '/login', account)
+  }
+
+  setToken(token: string) {
+    localStorage.setItem("token", token)
+  }
+
+  getToken(token: string) {
     return localStorage.getItem("token")
   }
 
 
+  }
 
-}
+
