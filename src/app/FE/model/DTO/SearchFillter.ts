a1fd92700d463733c5e844fddf061e06f0330825
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2023-01-09 14:11:57 +0700

    detail

---
+++ src/app/FE/model/DTO/SearchFillter.ts
@@ -0,0 +1,13 @@
+import {Provided} from "../Provided";
+
+export class SearchFillter {
+  id!: number
+  fullName!: string
+  img!: string
+  moTa!: string
+  provideds!: Provided[]
+  price!: number
+  city!: string
+  date!: Date
+  hires!:number
+}
