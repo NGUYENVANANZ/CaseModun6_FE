b775bd4be74468e8d1e4300fee3532c9844e50a2
Your Name <trangialochvtc@gmail.com>
2023-01-10 14:47:49 +0700

    add

--- src/app/Login-Register/register/register.component.ts
+++ src/app/Login-Register/register/register.component.ts
@@ -3,6 +3,8 @@ import {LoginService} from "../../service/login/login.service";
 import {Router} from "@angular/router";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import Swal from "sweetalert2";
+import {DetailAccount} from "../../FE/model/DetailAccount";
+
 // import {SignUpForm} from "../../FE/model/DTO/SignUpForm";
 @Component({
   selector: 'app-register',
@@ -10,9 +12,11 @@ import Swal from "sweetalert2";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  detailAccount !: DetailAccount
 
-  // checkDuplicateMail: boolean = true
-  // checkDuplicateUsername: boolean = true
+
+  checkDuplicateMail: boolean = true
+  checkDuplicateUsername: boolean = true
 
   constructor(private loginService: LoginService, private router: Router) {
   }
@@ -21,31 +25,35 @@ export class RegisterComponent implements OnInit {
   }
 
   registerForm = new FormGroup({
-    userName: new FormControl("", Validators.required),
-    email: new FormControl("", Validators.required),
-    password: new FormControl("", Validators.required),
-    confirmPassword: new FormControl("", Validators.required),
-    phoneNumber: new FormControl("", Validators.required),
-    birthday: new FormControl("", Validators.required),
-    gender: new FormControl("", Validators.required),
+    userName: new FormControl("", [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+    confirmPassword: new FormControl("", [Validators.required, Validators.minLength(6)]),
+    phoneNumber: new FormControl("", [Validators.required, Validators.pattern("(03|05|07|08|09)+([0-9]{8})")]),
+    gender: new FormControl("", Validators.nullValidator),
   })
 
 
   register() {
     this.loginService.register(this.registerForm.value).subscribe((data) => {
-        this.router.navigate([""])
+      this.checkDuplicateUsername = data[0];
+      this.checkDuplicateMail = data[1];
+      if (data[0] && data[1]) {
+        this.message()
+        this.router.navigate([" "])
+      }
     });
   }
 
-  // message() {
-  //   Swal.fire({
-  //     position: 'center',
-  //     icon: 'success',
-  //     title: 'Register account success ',
-  //     showConfirmButton: false,
-  //     timer: 1500
-  //   })
-  // }
+  message() {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Register account success ',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
 
   checkConfirmPassword() {
 
@@ -57,6 +65,9 @@ export class RegisterComponent implements OnInit {
       document.getElementById("abc").style.display = "none";
     }
   }
-}{
+
+
 
 }
+
+
