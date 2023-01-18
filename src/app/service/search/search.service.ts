b1d4d0a280245903e4affef18b3351d9970e16f5
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2023-01-18 12:29:11 +0700

    search filter

--- src/app/service/search/search.service.ts
+++ src/app/service/search/search.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {DetailAccountSart} from "../../FE/model/DTO/DetailAccountSart";
 import {DetailAccount} from "../../FE/model/DetailAccount";
@@ -23,4 +23,17 @@ export class SearchService {
     return this.http.get<DetailAccountSart[]>(`${API_URL}/showAll`);
   }
 
+
+  searchByAll(
+    gender:string,
+    birthday:number,
+    city:string)
+    : Observable<DetailAccountSart[]> {
+    let params = new HttpParams()
+      .set('gender',gender)
+      .set('birthday',birthday)
+      .set('city',city);
+
+    return this.http.get<DetailAccountSart[]>(`${API_URL}/search/searchFilter`, {params:params});
+  }
 }
